d1246e472f1201ade76d83ee28264d16427bf34e
Dariusrussellkish <russeldk@bc.edu>
2020-11-11 12:24:35 -0500

    initial push of antimodel data and jupyter notebook

--- moses/char_rnn/model.py
+++ moses/char_rnn/model.py
@@ -28,7 +28,9 @@ class CharRNN(nn.Module):
 
     def forward(self, x, lengths, hiddens=None):
         x = self.embedding_layer(x)
+        lengths = torch.as_tensor(lengths, dtype=torch.int64, device='cpu')
         x = rnn_utils.pack_padded_sequence(x, lengths, batch_first=True)
+        x.to(self.device)
         x, hiddens = self.lstm_layer(x, hiddens)
         x, _ = rnn_utils.pad_packed_sequence(x, batch_first=True)
         x = self.linear_layer(x)
---
+++ moses/char_rnn_am/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,4 @@
+from .config import get_parser as char_rnn_parser
+from .trainer import CharRNNTrainer
+
+__all__ = ['char_rnn_parser', 'CharRNNTrainer']
